a780fe8915529ff99542ab0efbf07c6ba52cc9be
SUBASH <121956112+GITHUB-SUBASHk@users.noreply.github.com>
2025-08-01 19:40:39 +0530

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // You can bind interfaces to implementations here
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // You can perform app bootstrapping here
+    }
+}
+?>
