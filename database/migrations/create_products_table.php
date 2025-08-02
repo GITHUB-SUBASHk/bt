e4ef239e9afa29fa197092d785c5d589f957a546
SUBASH <121956112+GITHUB-SUBASHk@users.noreply.github.com>
2025-08-02 19:54:03 +0530

    Update create_products_table.php

--- database/migrations/create_products_table.php
+++ database/migrations/create_products_table.php
@@ -1,22 +1,23 @@
 <?php
-{
-    Schema::create('users', function (Blueprint $table) {
-        $table->id();
-        $table->string('first_name');
-        $table->string('last_name');
-        $table->string('username')->unique();
-        $table->string('email')->unique();
-        $table->date('dob');
-        $table->string('languages');
-        $table->string('country');
-        $table->string('state');
-        $table->string('city');
-        $table->boolean('email_verified')->default(false);
-        $table->string('email_token')->nullable();
-        $table->string('otp')->nullable();
-        $table->string('password')->nullable(); // Will be null until verified
-        $table->rememberToken();
-        $table->timestamps();
-    });
-}
-?>
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
