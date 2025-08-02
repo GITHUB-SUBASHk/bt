3b9452d17b77e3861da586f0e5f65da2effba672
SUBASH <121956112+GITHUB-SUBASHk@users.noreply.github.com>
2025-08-02 15:22:46 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,10 +2,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Replace * with your Vercel domain in prod
+    'allowed_origins' => ['https://ft-4om7mtkq0-github-subashks-projects.vercel.app'], // Replace * with your Vercel domain in prod
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => false,
 ];
+
 ?>
